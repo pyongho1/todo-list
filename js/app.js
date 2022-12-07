31cdec69af3e53a313868f321f4091e5582727e4
Yong Park <pyongho0306@gmail.com>
2022-12-06 21:12:43 -0800

    Add eventlistener for add and reset

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
-console.log("test");
+// Cached element reference
+const inputField = document.getElementById("mainInput");
+const submitBtn = document.getElementById("submit-button");
+const todoList = document.getElementById("todo-list");
+const resetBtn = document.getElementById("reset-button");
+
+
+// ------- Main Listeners -------
+
+// Submit Button
+submitBtn.addEventListener("click", function (evt) {
+  const li = document.createElement("li");
+  li.textContent = inputField.value;
+  if (inputField.value === "") {
+    return null;
+  } else {
+    inputField.value = "";
+    document.querySelector("ul").appendChild(li);
+  }
+});
+
+// Reset Button
+resetBtn.addEventListener("click", function (evt) {
+  inputField.value = "";
+  document.querySelector("ul").innerHTML = "";
+});
